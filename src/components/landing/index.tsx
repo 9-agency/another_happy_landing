353d01480583af2bb386c798513a40c5cd751797
Luka Ušalj <luka@9.agency>
2020-12-13 22:01:49 +0100

    [PRS-415] redesigned landing

--- src/components/landing/index.tsx
+++ src/components/landing/index.tsx
@@ -2,15 +2,14 @@ import { Font, FontType } from "fonts";
 import React from "react";
 import styles from "./styles.module.css";
 
-const onContactClick = () => {
-  window.location.href = "mailto:hello@9.agency";
-};
-
 const LandingPage = () => (
   <div className={styles.container}>
-    <div className={styles.content_container}>
-      <Font fontType={FontType.headline1}>web development and design</Font>
-    </div>
+    <button
+      className={styles.button}
+      onClick={() => (window.location.href = "mailto:hello@9.agency")}
+    >
+      <Font fontType={FontType.headline1}>web design and development</Font>
+    </button>
   </div>
 );
 
