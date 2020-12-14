51888a0010c136f75d9556c0055e0fc5f71dc8e5
Luka Ušalj <luka@9.agency>
2020-12-14 23:18:02 +0100

    implemented multiple headlines

--- src/components/landing/index.tsx
+++ src/components/landing/index.tsx
@@ -2,19 +2,52 @@ import { Font, FontType } from "fonts";
 import React, { useEffect, useState } from "react";
 import styles from "./styles.module.css";
 
+const animationDuration = 700;
+const headerDisplayDuration = 7000;
+
 export const LandingPage = () => {
-  const [buttonClassName, setButtonClassName] = useState(styles.button);
+  const [buttonClassName, setButtonClassName] = useState(
+    `${styles.button} ${styles.zeroOpacity}`
+  );
+
+  const headlines = [
+    "web development done right",
+    "excellent piece of maintainable software",
+    "delivering solid products that scale",
+    "9 agency",
+  ];
+
+  const [activeHeadline, setActiveHeadline] = useState(headlines[0]);
 
   useEffect(() => {
     (document as any).fonts.ready.then(() => {
-      setButtonClassName((className) => `${className} ${styles.fadeIn}`);
+      setButtonClassName(`${styles.button} ${styles.fadeIn}`);
+
+      let changeHeadlineInterval: NodeJS.Timeout;
+
+      setTimeout(() => {
+        changeHeadlineInterval = setInterval(() => {
+          setButtonClassName(`${styles.button} ${styles.fadeOut}`);
+          setTimeout(() => {
+            setActiveHeadline((activeHeadline) => {
+              const activeHeadlineIndex = headlines.indexOf(activeHeadline);
+              return headlines[(activeHeadlineIndex + 1) % headlines.length];
+            });
+            setButtonClassName(`${styles.button} ${styles.fadeIn}`);
+          }, animationDuration);
+        }, headerDisplayDuration);
+      }, animationDuration);
+
+      return () => {
+        clearInterval(changeHeadlineInterval);
+      };
     });
   }, []);
 
   return (
     <div className={styles.container}>
       <a className={buttonClassName} href="mailto:hello@9.agency">
-        <Font fontType={FontType.headline1}>web design and development</Font>
+        <Font fontType={FontType.headline1}>{activeHeadline}</Font>
       </a>
     </div>
   );
