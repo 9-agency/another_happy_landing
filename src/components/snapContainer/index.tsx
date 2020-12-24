ce9bf59e3c1cdbdda1fc1f5d9401331316ae88b3
Luka Ušalj <luka@9.agency>
2020-12-24 01:19:16 +0100

    snap improvements

--- src/components/snapContainer/index.tsx
+++ src/components/snapContainer/index.tsx
@@ -1,9 +1,20 @@
+import { useEffect, useState } from "react";
 import styles from "./styles.module.css";
 
-export const SnapContainer = ({ children }: { [x: string]: any }) => {
-	return <div className={styles.snapContainer}>{children}</div>;
+export const SnapContainer = ({ children }: { children?: React.ReactNode }) => {
+  const [height, setHeight] = useState(window.innerHeight);
+
+  useEffect(() => {
+    setHeight(window.innerHeight);
+  }, []);
+
+  return (
+    <div className={styles.snapContainer} style={{ height }}>
+      {children}
+    </div>
+  );
 };
 
-export const SnapItem = ({ children }: { [x: string]: any }) => {
-	return <div className={styles.snapItem}>{children}</div>;
+export const SnapItem = ({ children }: { children?: React.ReactNode }) => {
+  return <div className={styles.snapItem}>{children}</div>;
 };
