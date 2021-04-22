38183f0d1af46c6aee428404941129782f4c154b
Luka Ušalj <luka@9.agency>
2021-04-22 13:11:38 +0200

    added section placeholders

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -1,9 +1,17 @@
-import React from "react";
-import { LandingPage } from "components/landing";
-import styles from "./styles.module.css";
+import ChessClock from "components/ChessClock";
+import Footer from "components/Footer";
+import HowWeWork from "components/HowWeWork";
+import Landing from "components/Landing";
+import Mantra from "components/Mantra";
+import V18 from "components/V18";
 
-export const App = () => (
-  <div className={styles.main_container}>
-    <LandingPage></LandingPage>
-  </div>
+export default () => (
+  <>
+    <Landing />
+    <V18 />
+    <HowWeWork />
+    <ChessClock />
+    <Mantra />
+    <Footer />
+  </>
 );
