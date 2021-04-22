95455a3bef7f0ad7d1f7944afc927de5933b7e56
Luka Ušalj <luka@9.agency>
2021-04-22 17:15:23 +0200

    implemented landing

--- src/components/Grid/index.tsx
+++ src/components/Grid/index.tsx
@@ -1,5 +1,5 @@
 export default ({ children }: { children?: React.ReactNode }) => (
-  <div style={{ display: "flex", justifyContent: "center" }}>
+  <div style={{ display: "flex", justifyContent: "center", width: "100%" }}>
     <div style={{ width: "100%", maxWidth: 820, margin: "0px 20px" }}>
       {children}
     </div>
--- src/components/landing/index.tsx
+++ src/components/landing/index.tsx
@@ -1,20 +1,34 @@
 import Flex from "components/Flex"
 import Grid from "components/Grid"
-import Logo from "components/Logo"
 import styles from "./styles.module.css"
+import { ReactComponent as Logo } from "assets/logo.svg"
+import { ReactComponent as IPhone } from "assets/iphone horizontal.svg"
 
 export default () => (
   <div className={styles.component}>
-    <header className={styles.header}>
+    <Header />
+    <Flex alignItemsCenter style={{ height: "75%" }}>
       <Grid>
-        <Flex justifyContentSpaceBetween alignItemsCenter>
-          <Logo />
-          <Flex className={styles.headerLabels}>
-            <p>PORTFOLIO</p>
-            <p>CONTACT</p>
-          </Flex>
+        <p style={{position: "relative"}} className={styles.headline}>Crafting awesome web <span style={{ color: "var(--accent)" }}>apps.</span></p>
+        <Flex style={{ position: "absolute", zIndex: -1, marginTop: -249, marginLeft: 105 }} justifyContentEnd>
+          <IPhone />
         </Flex>
       </Grid>
-    </header>
+    </Flex>
   </div>
 )
+
+const Header = () => (
+  <header className={styles.header}>
+    <Grid>
+      <Flex justifyContentSpaceBetween alignItemsCenter>
+        <Logo />
+        <Flex className={styles.headerLabels}>
+          <a href="#portfolio">PORTFOLIO</a>
+          <a href="mailto:hello@9.agency">CONTACT</a>
+        </Flex>
+      </Flex>
+    </Grid>
+  </header>
+)
+
