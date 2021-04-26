56ceadc35d6683d6701f5f3fb0cd3ed208a51e81
Luka Ušalj <luka@9.agency>
2021-04-26 16:34:12 +0200

    removed unused code

--- src/helpers.tsx
+++ src/helpers.tsx
@@ -1,12 +1,3 @@
 import React from "react";
 
-export function shuffleArray<A>(arrayToShuffle: A[]) {
-  const array = Array.from(arrayToShuffle);
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-  return array;
-}
-
 export const withIndexKey = (component: JSX.Element, index: number) => <React.Fragment key={index}>{component}</React.Fragment>
