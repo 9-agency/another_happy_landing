c39f1d34fbfb9c43b22c98fa118c6c30fe5db6b5
Luka Ušalj <luka@9.agency>
2021-05-07 18:23:25 +0200

    wip

--- src/components/BelowLanding/index.tsx
+++ src/components/BelowLanding/index.tsx
@@ -5,16 +5,38 @@ import Footer from "components/Footer";
 import ChessClock from "components/ChessClock";
 import NineAgency from "components/NineAgency";
 import Grid from "components/Grid";
+import { useEffect } from "react";
 
-export default () => (
-  <>
-    <div id="portfolio">
-      <Grid><h1>Featured projects.</h1></Grid>
-      <div id="react"><V18 /></div>
-      <div id="pwa"><ChessClock /></div>
-      <div id="codesplitting"><NineAgency /></div>
-    </div>
-    <div id="mantra"><Mantra /></div>
-    <div id="footer"><Footer /></div>
-  </>
-);
+const options = {
+  threshold: 0
+}
+
+const callback: IntersectionObserverCallback = (entries, observer) => {
+  entries.filter(entry => entry.isIntersecting).map(console.log)
+}
+
+const observer = new IntersectionObserver(callback, options)
+
+export default () => {
+  useEffect(() => {
+    document.querySelectorAll("#animateableSection").forEach(entry => {
+      observer.observe(entry)
+    })
+    return () => {
+    }
+  }, [])
+  return (
+    <>
+      <div id="portfolio">
+        <div id="animateableSection">
+          <Grid><h1>Featured projects.</h1></Grid>
+          <div id="react"><V18 /></div>
+        </div>
+        <div id="animateableSection"><div id="pwa"><ChessClock /></div></div>
+        <div id="animateableSection"><div id="codesplitting"><NineAgency /></div></div>
+      </div>
+      <div id="animateableSection"><div id="mantra"><Mantra /></div></div>
+      <div id="animateableSection"><div id="footer"><Footer /></div></div>
+    </>
+  );
+}
