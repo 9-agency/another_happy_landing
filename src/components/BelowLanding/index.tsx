8d5bc7ad7357250e0e1451b008a9a0f3a0d47cf9
Luka Ušalj <luka.usalj@q.agency>
2021-05-08 15:56:21 +0200

    encapsulated intersectionObserverSetup

--- src/components/BelowLanding/index.tsx
+++ src/components/BelowLanding/index.tsx
@@ -6,47 +6,11 @@ import ChessClock from "components/ChessClock";
 import NineAgency from "components/NineAgency";
 import Grid from "components/Grid";
 import { useEffect } from "react";
-
-const options: IntersectionObserverInit = {}
-
-const translateIntersectionObserverCallback: IntersectionObserverCallback = (entries, _observer) => {
-  entries.filter(entry => entry.isIntersecting).map(entry => {
-    (entry as any).target.style.animation = "translateSectionAnimation .6s forwards ease-out";
-  })
-}
-
-const translateIntersectionObserver = new IntersectionObserver(translateIntersectionObserverCallback, options)
-
-const scaleIntersectionObserverCallback: IntersectionObserverCallback = (entries, _observer) => {
-  entries.filter(entry => entry.isIntersecting).map(entry => {
-    (entry as any).target.style.animation = "scaleSectionAnimation .2s forwards ease-out";
-  })
-}
-
-const scaleIntersectionObserver = new IntersectionObserver(scaleIntersectionObserverCallback, { threshold: 0.2 })
-
-const fadeInIntersectionObserverCallback: IntersectionObserverCallback = (entries, _observer) => {
-  entries.filter(entry => entry.isIntersecting).map(entry => {
-    (entry as any).target.style.animation = "fadeInSectionAnimation .5s .5s forwards ease-out";
-  })
-}
-
-const fadeInIntersectionObserver = new IntersectionObserver(fadeInIntersectionObserverCallback, options)
+import intersectionObserverSetup from "intersectionObserverSetup";
 
 export default () => {
-  useEffect(() => {
-    document.querySelectorAll("#translateSection").forEach(entry => {
-      translateIntersectionObserver.observe(entry)
-    })
-    document.querySelectorAll("#scaleSection").forEach(entry => {
-      scaleIntersectionObserver.observe(entry)
-    })
-    document.querySelectorAll("#fadeInSection").forEach(entry => {
-      fadeInIntersectionObserver.observe(entry)
-    })
-    return () => {
-    }
-  }, [])
+  useEffect(intersectionObserverSetup, [])
+  
   return (
     <>
       <div id="portfolio">
