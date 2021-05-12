f29756d8c7362da59cc5fbd6d226decc072ab782
David <david@9.agency>
2021-05-12 19:22:52 +0200

    neznam

--- src/components/Grid/index.tsx
+++ src/components/Grid/index.tsx
@@ -1,6 +1,6 @@
 export default ({ children, className, style }: { children?: React.ReactNode, className?: string, style?: React.CSSProperties }) => (
   <div style={{ display: "flex", justifyContent: "center", width: "100%"}} {...{ className }}>
-    <div style={{ width: "100%", maxWidth: 820, margin: "0px 20px" , ...style }}>
+    <div style={{ width: "100%", maxWidth: 1000, margin: "0px 20px" , ...style }}>
       {children}
     </div>
   </div>
